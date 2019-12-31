4653a9affe602f6ce7047528e7ab772dc71125c4
bbd <WenBangbbd@outlook.com>
2019-12-31 16:33:40 +0800

    添加掩码点和地址类，输入点分十进制获取位数，输入位数获取点分十进制地址

---
+++ IPAddCalculator.UnitTest/AddrressTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using IPAddCalculator.Model;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+
+namespace IPAddCalculator.UnitTest
+{
+    public class AddrressTest
+    {
+        [Test]
+        public void GetAddrress_InputString_ReturnNotNull()
+        {
+            string strAddr = "192.168.140.123";
+            Addrress addr = new Addrress(strAddr);
+            Assert.IsNotNull(addr);
+            Assert.AreEqual(strAddr, addr.StrAddr);
+        }
+        [Test]
+        public void AddrSegments_WhenGetAddress_AddrSegmentsIsRelationWithStrAddr()
+        {
+            string strAddr = "123.234.121.213";
+            Addrress addr = new Addrress(strAddr);
+            Assert.AreEqual(123, addr.AddrSegments[0]);
+            Assert.AreEqual(234, addr.AddrSegments[1]);
+            Assert.AreEqual(121, addr.AddrSegments[2]);
+            Assert.AreEqual(213, addr.AddrSegments[3]);
+        }
+
+        [TestCase("1231.23.12.1")]
+        [TestCase("1231.23.12.-1")]
+        public void GetAddress_IfStrAddrIsInvilid_ThrwoException(string strAddr)
+        {
+            var ex = Assert.Catch<FormatException>(() => new Addrress(strAddr));
+            StringAssert.Contains("字符串地址格式不正确", ex.Message);
+        }
+    }
+}
---
+++ IPAddCalculator.UnitTest/MaskTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IPAddCalculator.Model;
+
+namespace IPAddCalculator.UnitTest
+{
+    class MaskTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+        [Test]
+        public void GetAMaskByString_ReturnNotNull()
+        {
+            string addr = "255.255.255.248";
+            Mask mask = new Mask(addr);
+            Assert.IsNotNull(mask);
+            Assert.AreEqual(addr, mask.Addr.StrAddr);
+        }
+
+        [TestCase("255.255.1.248")]
+        [TestCase("255.1.0.0")]
+        public void GetAMaskByString_IfInputCanNotCreateAMask_ThrowException(string strAddr)
+        {
+            var ex = Assert.Catch<FormatException>(() => new Mask(strAddr));
+            StringAssert.Contains("地址是无效掩码", ex.Message);
+        }
+
+        [TestCase("255.255.255.248", 29)]
+        [TestCase("255.0.0.0", 8)]
+        public void GetMaskBit_ReturnMaskBit(string maskAddr, int maskBit)
+        {
+            Mask mask = new Mask(maskAddr);
+            Assert.AreEqual(maskBit, mask.Bit);
+        }
+        [TestCase(0,"0.0.0.0")]
+        [TestCase(16, "255.255.0.0")]
+        public void GetMaskByInt_AddrIsRelationWithInputInt(int maskBit,string strMaskAddr)
+        {
+            Mask mask=new Mask(maskBit);
+            Assert.AreEqual(strMaskAddr,mask.Addr.StrAddr);
+        }
+    }
+}
